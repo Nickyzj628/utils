aa895ca4262346337797e7bfef3b81afbb783d44
Nickyzj <1927139831@qq.com>
2025-12-01 18:55:32 +0800

    feat(is): 添加 isPrimitive

--- src/is.ts
+++ src/is.ts
@@ -1,8 +1,19 @@
 /**
  * 检测传入的值是否为**普通对象**
- * @param value
- * @returns {Boolean} 如果是普通对象，返回 true，否则返回 false
+ * @returns 如果是普通对象，返回 true，否则返回 false
  */
 export const isObject = (value: any): value is object => {
   return value?.constructor === Object;
+};
+
+/**
+ * 检测传入的值是否为**原始值**（number、string、boolean、symbol、bigint、undefined、null）
+ * @returns 如果是原始值，返回 true，否则返回 false
+ */
+export const isPrimitive = (value: any) => {
+  return (
+    value === null ||
+    value === undefined ||
+    (typeof value !== "object" && typeof value !== "function")
+  );
 };
