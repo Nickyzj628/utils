77a347963aef04f42d2ea6365f3bb303aba35e2e
Nickyzj <1927139831@qq.com>
2026-01-30 23:40:07 +0800

    fix: 修复上次提交时未打包的问题

--- dist/string.d.ts
+++ dist/string.d.ts
@@ -1,31 +1,35 @@
+export type SnakeToCamel<S extends string> = S extends `${infer P1}_${infer P2}${infer P3}` ? `${P1}${Capitalize<SnakeToCamel<`${P2}${P3}`>>}` : S;
 /**
  * 下划线命名法转为驼峰命名法
  *
  * @example
  * snakeToCamel("user_name") // "userName"
  */
-export declare const snakeToCamel: (str: string) => string;
+export declare const snakeToCamel: <S extends string>(str: S) => SnakeToCamel<S>;
+export type CamelToSnake<S extends string> = S extends `${infer P1}${infer P2}${infer Rest}` ? P2 extends Capitalize<P2> ? `${P1}_${Lowercase<P2>}${CamelToSnake<Rest>}` : `${P1}${CamelToSnake<`${P2}${Rest}`>}` : S;
 /**
  * 驼峰命名法转为下划线命名法
  *
  * @example
  * camelToSnake("shouldComponentUpdate") // "should_component_update"
  */
-export declare const camelToSnake: (str: string) => string;
+export declare const camelToSnake: <S extends string>(str: S) => CamelToSnake<S>;
+export type Capitalize<S extends string> = S extends `${infer P1}${infer Rest}` ? P1 extends Capitalize<P1> ? S : `${Uppercase<P1>}${Rest}` : S;
 /**
  * 字符串首字母大写
  *
  * @example
  * capitalize("hello") // "Hello"
  */
-export declare const capitalize: (s: string) => string;
+export declare const capitalize: <S extends string>(s: S) => Capitalize<S>;
+export type Decapitalize<S extends string> = S extends `${infer P1}${infer Rest}` ? P1 extends Lowercase<P1> ? P1 : `${Lowercase<P1>}${Rest}` : S;
 /**
  * 字符串首字母小写
  *
  * @example
  * decapitalize("Hello") // "hello"
  */
-export declare const decapitalize: (s: string) => string;
+export declare const decapitalize: <S extends string>(s: S) => Decapitalize<S>;
 /**
  * 图片地址转 base64 数据
  *
